11b1f7044415590edcf051a7526f8d149ac078d2
oliullah05 <mdolihasan@gmail.com>
2024-04-23 16:17:18 +0600

    Show Off All The Fantastic Blogs Using SSR

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,21 @@
+import BlogCard from "@/components/ui/BlogCard";
+
+const AllBlogsPage = async () => {
+    const res = await fetch("http://localhost:5000/blogs",{
+        cache:"no-store"
+    })
+    const blogs: TBlog[] = await res.json()
+    return (
+        <div>
+            <h1 className="text-center text-4xl my-5">All Blogs From <span className='text-accent'>Blogiz</span></h1>
+            <p className='text-center text-xl text-gray-400 italic w-2/4 mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab expedita at eligendi ducimus! Id quisquam magni repudiandae qui aliquid quis inventore ad dolorum, exercitationem excepturi!</p>
+            <div className="grid grid-cols-3 gap-4 w-[90%] mx-auto my-5">
+                {
+                    blogs.map((blog) => <BlogCard key={blog.id} blog={blog}></BlogCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default AllBlogsPage;
